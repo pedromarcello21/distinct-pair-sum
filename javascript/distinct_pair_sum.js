ae0e7b04ddee206f8e93dcc575abea155bb9aaec
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-10-01 09:08:03 -0400

    solution

--- javascript/distinct_pair_sum.js
+++ javascript/distinct_pair_sum.js
@@ -1,5 +1,25 @@
 function distinctPairSum(arr, k) {
   // type your code here
+  //establish the list of pairs to be returned
+  const pairs = []
+  //create a set to keep track of unique values in the provided array
+  let seen = new Set()
+  for(let i = 0; i<arr.length; i++){
+    //define complement to value in array to equate to k
+    const complement = k-arr[i]
+    //if this complement value has been seen
+    if (seen.has(complement)){
+      //define the pair to be lesser then greater
+      let pair = [Math.min(complement, arr[i]), Math.max(complement, arr[i])]
+      //check values in pairs to see if corresponding index values are in pairs
+      if (!pairs.some(existingPair => existingPair[0] === pair[0] && existingPair[1] === pair[1])) {
+        pairs.push(pair);
+      }
+    }
+    //add value to seen to keep track in next loop
+    seen.add(arr[i])
+  }
+  return pairs
 }
 
 if (require.main === module) {
